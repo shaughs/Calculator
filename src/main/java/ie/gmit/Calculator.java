30f57ca30c6b5bc88e76de833aa7b8644bfea84c
Sean <g00358883@gmit.ie>
2021-02-17 12:44:52 +0000

    Removing

--- src/main/java/ie/gmit/Calculator.java
+++ src/main/java/ie/gmit/Calculator.java
@@ -1,10 +1,25 @@
 package ie.gmit;
 
 public class Calculator {
-//This method will add two numbers
+//These methods will add,subtract,divide and multiply two numbers
 
     public int add(int number1, int number2){
-        int result = number1 + number2;
-        return result;
+        int addResult = number1 + number2;
+        return addResult;
+    }
+
+    public int subtract(int number1, int number2){
+        int subtractResult = number1 - number2;
+        return subtractResult;
+    }
+
+    public int divide(int number1, int number2){
+        int divideResult = number1 / number2;
+        return divideResult;
+    }
+
+    public int multiply(int number1, int number2){
+        int multiplyResult = number1 * number2;
+        return multiplyResult;
     }
 }
