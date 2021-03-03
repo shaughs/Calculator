bc40dcacd5a2128804ce15d3705a2fec827f516f
Sean <g00358883@gmit.ie>
2021-03-03 13:22:55 +0000

    Adding test methods

--- src/main/java/ie/gmit/Calculator.java
+++ src/main/java/ie/gmit/Calculator.java
@@ -4,22 +4,39 @@ public class Calculator {
 //These methods will add,subtract,divide and multiply two numbers
 
     public int add(int number1, int number2){
-        int addResult = number1 + number2;
-        return addResult;
+        if(number1 > 0 && number2 > 0) {
+            int addResult = number1 + number2;
+            return addResult;
+        }else{
+            throw new IllegalArgumentException("Not a valid Entry");
+        }
     }
 
     public int subtract(int number1, int number2){
-        int subtractResult = number1 - number2;
-        return subtractResult;
+        if(number1 > 0 && number2 > 0) {
+            int subtractResult = number1 - number2;
+            return subtractResult;
+        }else{
+            throw new IllegalArgumentException("Not a valid Entry");
+        }
     }
 
     public int divide(int number1, int number2){
-        int divideResult = number1 / number2;
-        return divideResult;
+        if(number1 > 0 && number2 > 0) {
+            int divideResult = number1 / number2;
+            return divideResult;
+        }else{
+            throw new IllegalArgumentException("Not a valid Entry");
+        }
     }
 
     public int multiply(int number1, int number2){
-        int multiplyResult = number1 * number2;
-        return multiplyResult;
+            if(number1 > 0 && number2 > 0) {
+                int multiplyResult = number1 * number2;
+                return multiplyResult;
+            }else{
+                throw new IllegalArgumentException("Not a valid Entry");
+            }
     }
+
 }
